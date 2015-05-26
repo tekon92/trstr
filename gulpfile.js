c17f698a55ee6f5bd1769024b4db9195022b1677
tekon92 <tekon92@gmail.com>
2015-05-26 22:44:36 +0700

    single-post

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var plumber = require('gulp-plumber');
 var autoprefixer = require('gulp-autoprefixer');
 var notify = require('gulp-notify');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass', function(){
 gulp.src('./scss/{,*/}*{scss,sass}')
@@ -15,9 +16,11 @@ gulp.src('./scss/{,*/}*{scss,sass}')
 	}))
 	.pipe(sourcemaps.write())
 	.pipe(notify("compiled successful"))
-	.pipe(gulp.dest('./css'));
+	.pipe(gulp.dest('./css'))
+	.pipe(livereload());
 })
 
 gulp.task('default', ['sass'], function(){
+	livereload.listen();
 	gulp.watch('./scss/{,*/}*.{scss,sass}', ['sass'])
 });
